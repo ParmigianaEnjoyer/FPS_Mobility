1380357977cd92e4088758d8a6d47f52288ea187
ParmigianaEnjoyer <nico02r@gmail.com>
2024-06-01 16:13:52 +0200

    risolto il problema della posizione delle particelle

--- Scripts/blood.gd
+++ Scripts/blood.gd
@@ -4,6 +4,7 @@ extends Node3D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$GPUParticles3D.emitting = true
+	look_at(get_tree().get_first_node_in_group("player").global_position)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
