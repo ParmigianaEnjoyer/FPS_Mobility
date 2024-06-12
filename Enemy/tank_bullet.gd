3e61b3ee4753c1f5a2bf3fa8e85917176b2974f6
ParmigianaEnjoyer <nico02r@gmail.com>
2024-06-12 11:29:35 +0200

    migliorato il proiettile del tank e aumentato il suo range

--- Enemy/tank_bullet.gd
+++ Enemy/tank_bullet.gd
@@ -7,7 +7,7 @@ const SPEED = 30.0
 @onready var lifetime = $Timer
 
 var damage = 10
-var bullet_range = 20
+var bullet_range = 40
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -20,11 +20,6 @@ func _process(delta):
 	position += transform.basis * Vector3(0, 0, -SPEED) * delta
 	ray.force_raycast_update()
 
-	if ray.is_colliding() and ray.get_collider().is_in_group("player"):
-		mesh.visible = false
-		ray.get_collider().take_damage(damage)
-		queue_free()
-
 
 func calculate_bullet_lifetime(the_bullet_range):
 	if SPEED != 0:
@@ -33,3 +28,9 @@ func calculate_bullet_lifetime(the_bullet_range):
 
 func _on_timer_timeout():
 	queue_free()
+
+
+func _on_area_3d_body_entered(body):
+	if body.is_in_group("player"):
+		body.take_damage(damage)
+		queue_free()
